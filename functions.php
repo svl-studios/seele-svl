0d49ec4f5ebbdd57550c0b7ace5bf8d0dc6bd283
Kevin Provance <kevin.provance@gmail.com>
2021-02-26 18:34:11 -0500

    Redo for server side theme functions.

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function svl_setup() {
 add_action( 'after_setup_theme', 'svl_setup', 9 );
 
 require_once get_stylesheet_directory() . '/admin/class-qixi-functions.php';
-
+echo 'bitch';
 /**
  * Run SVL Votes AJAX
  */
