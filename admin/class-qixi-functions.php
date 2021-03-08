e128a9e6441ce2169a11c967be960943faefe703
Kevin Provance <kevin.provance@gmail.com>
2021-03-07 21:46:41 -0500

    Add nonce create, activate, decative, and validate.

--- admin/class-qixi-functions.php
+++ admin/class-qixi-functions.php
@@ -5,6 +5,45 @@ if ( ! class_exists( 'Qixi_Functions' ) ) {
 		public function __construct() {
 			add_action( 'wp_ajax_qixi_get_download', array( $this, 'download_plugin' ) );
 			add_action( 'wp_ajax_nopriv_qixi_get_download', array( $this, 'download_plugin' ) );
+
+			add_action( 'wp_ajax_qixi_deactivate_license', array( $this, 'deactivate' ) );
+			add_action( 'wp_ajax_nopriv_qixi_deactivate_license', array( $this, 'deactivate' ) );
+
+			add_action( 'wp_ajax_qixi_activate_license', array( $this, 'activate' ) );
+			add_action( 'wp_ajax_nopriv_qixi_activate_license', array( $this, 'activate' ) );
+
+			add_action( 'wp_ajax_qixi_validate_token', array( $this, 'validate' ) );
+			add_action( 'wp_ajax_nopriv_qixi_validate_token', array( $this, 'validate' ) );
+
+			add_action( 'wp_ajax_qixi_create_nonce', array( $this, 'nonce' ) );
+			add_action( 'wp_ajax_nopriv_qixi_create_nonce', array( $this, 'nonce' ) );
+		}
+
+		public function nonce() {
+			if ( $_GET['action'] ) {
+				$key = sanitize_text_field( wp_unslash( $_GET['action'] ?? '' ) );
+
+				echo sanitize_key( wp_unslash( wp_create_nonce( $key ) ) );
+			}
+		}
+
+		public function activate() {
+			$key      = sanitize_text_field( wp_unslash( $_GET['key'] ?? '' ) );
+			$product  = sanitize_text_field( wp_unslash( $_GET['product'] ?? '' ) );
+			$site_url = sanitize_text_field( wp_unslash( $_GET['site_url'] ?? '' ) );
+
+		}
+
+		public function deactivate() {
+			$token    = sanitize_text_field( wp_unslash( $_GET['token'] ?? '' ) );
+			$site_url = sanitize_text_field( wp_unslash( $_GET['$site_url'] ?? '' ) );
+
+		}
+
+		public function validate() {
+			$token    = sanitize_text_field( wp_unslash( $_GET['token'] ?? '' ) );
+			$site_url = sanitize_text_field( wp_unslash( $_GET['$site_url'] ?? '' ) );
+
 		}
 
 		public function download_plugin(){
